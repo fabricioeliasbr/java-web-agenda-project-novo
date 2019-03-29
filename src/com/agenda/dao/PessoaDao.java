292d0b4ef3468eaa80ee333c09fac6f78a87c642
Fabrício Elias <fabricio.silva226@etec.sp.gov.br>
2019-03-28 22:23:14 -0300

    Inserimos a string "SQL" a classe PessoaDAO.java, criamos a package "util" e inserimos uma public na ConnectionFactory.java

--- src/com/agenda/dao/PessoaDao.java
+++ src/com/agenda/dao/PessoaDao.java
@@ -1,15 +1,18 @@
 package com.agenda.dao;
 
 import com.agenda.modelos.Pessoas;
+import com.mysql.jdbc.Connection;
 
 public class PessoaDao {
+	private Connection connection;
+	
 	public void cadastraDAO(Pessoas pessoa) {
-		
-		System.out.println(pessoa.getNome());
+		String SQL = "insert into pessoas (email, endereço, id, nome, telefone) values (?,?,?,?,?)";
+		/*System.out.println(pessoa.getNome());
 		System.out.println(pessoa.getSenha());
 		System.out.println(pessoa.getEmail());		
 		System.out.println(pessoa.getTelefone());
-		System.out.println(pessoa.getEndereco());
+		System.out.println(pessoa.getEndereco());*/
 	}
 
 }
